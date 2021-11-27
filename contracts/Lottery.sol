1bbaa90159972a2ff3e081af2f4485ac62d04a10
Zoe Schmitt <zoeschmitt@hotmail.com>
2021-11-27 12:19:09 -0600

    added randomness using chainlink

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery is Ownable {
+contract Lottery is Ownable, VRFConsumerBase {
     enum LOTTERY_STATE {
         OPEN,
         CLOSED,
@@ -15,11 +16,22 @@ contract Lottery is Ownable {
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
     LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyhash;
+    address public recentWinner;
 
-    constructor(address _priceFeedAddress) public {
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
@@ -58,6 +70,24 @@ contract Lottery is Ownable {
         //         )
         //     ) % players.length
         // );
-        
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet."
+        );
+        require(_randomness > 0, "random-not-found");
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        // 7 players
+        // 22
+        // 22 % 7 = 1
+        // 7 
     }
 }
